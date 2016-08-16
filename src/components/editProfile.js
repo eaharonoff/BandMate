c99eb6cfa918058b3b0b08038206ebdbfb033bde
leearaneta <lee.araneta@gmail.com>
2016-08-16 00:01:02 -0400

    created userProfileContainer

--- src/components/editProfile.js
+++ src/components/editProfile.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
-class EditUser extends Component {
+class EditProfile extends Component {
   render() {
     return (
       <form id='user-edit-form'>
-      
-       <input type='text' />
+       <input type='text'/>
       </form>
     )
   }
@@ -17,6 +15,4 @@ function mapStateToProps(state) {
   return {currentUser: state.currentUser}
 }
 
-var SmartEdit = connect(mapStateToProps)(EditUser)
-
-export default SmartEdit;
+export default connect(mapStateToProps)(EditProfile)
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,26 +1,16 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
-import SmartTitle from './title';
-import Details from './details';
-import SmartEdit from './editUser';
+import React from 'react';
 
-class Profile extends Component {
-  render() {
-    return (
-      <div id='user-profile'>
-       <SmartTitle />
-       <Details />
-       <Link to='/profile/edit'> Edit profile</Link>
-      </div>
-    )
-  }
+const Profile = ({data}) => {
+  return (
+    <div>
+      <h3>{data.email}, {data.age}</h3>
+      <div>Zip code: {data.zip}</div>
+      <div>Bio:{data.bio}</div>
+      <div>Instruments:{data.instruments.map(instrument => instrument.name)}</div>
+      <div>Genres:{data.genres.map(genre => genre.name)}</div>
+      <div>Music Goes Here:{data.music}</div>
+    </div>
+  )
 }
 
-function mapStateToProps(state) {
-  return {currentUser: state.currentUser}
-}
-
-var SmartProfile = connect(mapStateToProps)(Profile)
-
-export default SmartProfile;
+export default Profile
