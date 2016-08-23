254a9c3fa90d8e51d6d40b6b4ed6a9a635d729ea
Dan <dandanberry@gmail.com>
2016-08-23 09:48:59 -0400

    leftover from design changes

--- src/components/basics.js
+++ src/components/basics.js
@@ -4,16 +4,18 @@ import React from 'react'
 const Basics = ({data, viewProfile}) => {
   !data.picture ? data.picture = 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg' : null
   return (
-    <div>
-      <img className='img-rounded' src={data.picture} height='100' width='100'></img>
-      <a href="#" onClick={viewProfile} id={data.id}>{data.name}, {data.age}</a>
-      <div>City: {data.city.name}</div>
-      <div>Instruments: {data.instruments.map(instrument => instrument.name).join(", ")}</div>
+    <div className="basics">
+
+      <a href="#" onClick={viewProfile} id={data.id}>{data.name}, {data.age}</a><br/>
+      <div>City: {data.city.name}</div><br/>
+      <div>Instruments: {data.instruments.map(instrument => instrument.name).join(", ")}</div><br/>
       <div>Genres: {data.genres.map(genre => genre.name).join(", ")}</div>
     </div>
   )
 }
 export default Basics
 
+
+// <img className='img-rounded' src={data.picture} height='100' width='100'></img>
 // <div>Instruments: {data.instruments.map(instrument => instrument.name).join(", ")}</div>
 // <div>Genres: {data.genres.map(genre => genre.name).join(", ")}</div>
