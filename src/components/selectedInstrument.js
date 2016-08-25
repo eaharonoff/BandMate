6e9004015bd8f6aceb45771750717d6afda8fe1d
RKoopman <raoul.koopman@gmail.com>
2016-08-25 00:08:18 -0400

    edited selected genre and instrument forms

--- src/components/selectedInstrument.js
+++ src/components/selectedInstrument.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const SelectedInstrument = ({instrument, remove}) => {
   return (
-    <div>
-      {instrument} <a data-instrument={instrument} onClick={remove} href='#'>remove</a>
-    </div>
+    <center>
+      <div className="selected-style">
+        {instrument} <a data-instrument={instrument} onClick={remove} href='#'>remove</a>
+      </div>
+    </center>
   )
 }
 export default SelectedInstrument
